a940c140d9813bf08813e0b4c72a33d60b1c8dd9
Raver Miradora <ravermiradora0@gmail.com>
2023-12-03 18:52:33 +0800

    Updated files with new changes

--- src/com/company/CalendarTester.java
+++ src/com/company/CalendarTester.java
@@ -1,3 +1,19 @@
+/*
+ * Calendar-Event-Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Calendar-Event-Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Calendar-Event-Manager. If not, see 
+ * https://github.com/Raver-Miradora/Calendar-Event-Manager
+ */
+
 package com.company;
 
 /**
